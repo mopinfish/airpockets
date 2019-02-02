6847e0d882965b4584c5279f491b0750aaff77ba
OtsukaNoboru <mopinfish@gmail.com>
2019-02-02 18:17:33 +0900

    [airpockets] remove console errors by refactor content-security-policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,16 +9,16 @@ Rails.application.config.content_security_policy do |policy|
   policy.font_src    :self, :https, :data
   policy.img_src     :self, :https, :data
   policy.object_src  :none
-  policy.script_src  :self, :https
-  policy.style_src   :self, :https
 
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval
-    policy.default_src :self, :https, :unsafe_eval
+    policy.default_src :self, :https
     policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
+    policy.style_src   :self, :https, :unsafe_inline
   else
     policy.script_src :self, :https
     policy.default_src :self, :https
+    policy.style_src   :self, :https
   end
   # Specify URI for violation reports
   # policy.report_uri "/csp-violation-report-endpoint"
